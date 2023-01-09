f76743caa831f44d1ee48f52bc378b5f2b965511
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2023-01-09 22:18:14 +0100

    commit new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const images = [
+  {
+    src: "https://www.pixelstalk.net/wp-content/uploads/images6/HD-Wallpaper-Kyrie-Irving.jpg",
+    name: "Kyrie"
+  },
+
+  {
+    src: "https://images.unsplash.com/photo-1673208839785-d0d3df5b39c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Mos design"
+  },
+
+  {
+    src: "https://images.unsplash.com/photo-1673205194019-55ac9f0a0899?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Third"
+  },
+
+  {
+    src: "https://images.unsplash.com/photo-1673232141234-889ed2cf341c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+    name: "City"
+  },
+
+  {
+    src: "https://images.unsplash.com/photo-1673209377892-222ee5e19e7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+    name: "iceburg"
+  }
+]
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.add.bind(this), 2000)
+  }
+
+  sub() {
+    const count = this.state.count - 1
+    this.setState({ count: count < 0 ? images.length - 1 : count })
+  }
+
+  add() {
+    this.setState({ count: (this.state.count + 1) % images.length })
+  }
+
+  render() {
+    return <div className='app'>
+      <img src={images[this.state.count].src} alt={images[this.state.count].name} id='image' />
+      <p>{this.state.count + 1}. {images[this.state.count].name}</p>
+      <div className='buttonFld'>
+        <button onClick={this.sub.bind(this)}>&lang;</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.add.bind(this)}>&rang;</button>
+      </div>
+      <div className='imageCards'>
+        {
+          images.map((image) => {
+            return <div className='card'>
+              <img src={image.src} alt={images.name} id='image' />
+              <p> { } </p>
+            </div>
+          })
+        }
+      </div>
     </div>
-  );
+  }
 }
-
-export default App;
